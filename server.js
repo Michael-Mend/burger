926381a3b004c579ffeb7bd296b10c86e7ba9d48
Michael Mendoza <michael.mend97@yahoo.com>
2020-05-13 15:13:05 -0400

    !

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 
 app.use(express.static("public"));
 
-app.use(express.unlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 var exphbs = require("express-handlebars");
